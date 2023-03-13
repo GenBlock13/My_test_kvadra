a286e070c68ba5ed41cf65a88f3ae3deb32a36ab
BlockGen13 <blockgen@yandex.ru>
2023-03-13 21:12:06 +0300

    password

--- scrathes/my_input.py
+++ scrathes/my_input.py
@@ -1,1 +1,23 @@
 # hello my_input
+
+
+def my_password():
+    word = input()
+    password = ''
+    for i in word:
+        password += chr(ord(i) + pow(3, 2))
+    return password
+
+
+def unpass(password):
+    key = ''
+    for i in password:
+        key += chr(ord(i) - pow(3, 2))
+    return key
+
+ps = my_password()
+
+back = unpass(ps)
+
+print(ps)
+print(back)
