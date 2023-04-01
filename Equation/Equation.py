044d2630f8c1c0f3bc3153e2ce146f8c087c6d76
GenBlock13 <blockgen@yandex.ru>
2023-04-01 14:29:26 +0300

    it works

--- Equation/Equation.py
+++ Equation/Equation.py
@@ -18,61 +18,16 @@ class QuadraticEquation:
             return x1, x2
         elif D == 0:
             x = -self.b / (2 * self.a)
-            return x, None
+            return x, x
         else:
             x_real = -self.b / (2 * self.a)
             x_imag = math.sqrt(-D) / (2 * self.a)
             x1 = x_real + x_imag * 1j
             x2 = x_real - x_imag * 1j
-            return None, x1, x2
-        from math import sqrt
-        from cmath import sqrt
-
-        def solve_linear(b, c):
-            if b == 0 and c == 0:
-                linear_root = "Many roots"
-                return linear_root
-            if b == 0:
-                linear_root = "Can't be solved"
-                return linear_root
-            else:
-                linear_root = (-c / b)
-            return linear_root
-
-        def solve_discrim(a, b, c):
-            dscrm = b * b - 4 * a * c
-            x1 = 0
-            x2 = 0
-            if dscrm < 0:
-                x1 = str((-b - sqrt(dscrm)) / (2 * a))
-                x2 = str((-b + sqrt(dscrm)) / (2 * a))
-                x1 = x1.replace('j', 'i')
-                x2 = x2.replace('j', 'i')
-                return x1, x2
-            elif dscrm == 0:
-                x1 = (-b) / (2 * a)
-                x2 = (-b) / (2 * a)
-                return x1, x2
-            else:
-                x1 = ((-b) - dscrm ** 0.5) / (2 * a)
-                x2 = ((-b) + dscrm ** 0.5) / (2 * a)
-                return x1, x2
-
-        def solve_equation(a, b, c):
-            if a == 0:
-                return solve_linear(b, c)
-            else:
-                return solve_discrim(a, b, c)
-
-
-class QuadraticOutput:
-    def __init__(self, roots):
-        self.roots = roots
+            x1 = str(x1).replace('j', 'i')
+            x2 = str(x2).replace('j', 'i')
+            return  x1, x2
 
     def __str__(self):
-        if self.roots[0] is not None:
-            x1, x2 = self.roots
-            return f"The roots are {x1:.2f} and {x2:.2f}."
-        else:
-            x = self.roots[1]
-            return f"The root is {x:.2f}."
+        return 'Root of equation are: {0} '.format(self.roots())
+
